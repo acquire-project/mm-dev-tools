cfd920922b509b4cbde7e61de2e95f64f8be0611
namodaj <nenad@amodaj.com>
2024-01-25 17:06:52 -0800

    paths changed

--- build-acquire.bat
+++ build-acquire.bat
@@ -2,22 +2,22 @@ Rem assuming we are running this in the Visual Studio command line environment
 
 Rem build acquire
 set current-dir=%cd%
-set acquire-build-dir=C:\projects\czi\acquire-video-runtime\build
-set acquire-install-dir=C:\projects\czi\acquire-video-runtime\install
+set acquire-build-dir=C:\projects\czi\acquire-common\build
+set acquire-install-dir="C:/Program Files (x86)/acquire-common"
 set mm-3rdparty-dir=C:\projects\3rdparty\CZI\acquire
 set mm-adapter-dir=C:\projects\micro-manager\mmCoreAndDevices\DeviceAdapters\Acquire
 set mm-runtime-dir=C:\Program Files\Micro-manager-2.0
-set config=Debug
-set acq-target=Build
+set config=Release
+set acq-target=Rebuild
 set mm-target=Rebuild
 set platform=x64
 
 cd %acquire-build-dir%
-msbuild INSTALL.vcxproj /t:%acq-target% /p:Configuration=%config% /p:Platform=%platform%"
+msbuild INSTALL.vcxproj /t:%acq-target% /p:Configuration=%config% /p:Platform=%platform%
 
 Rem copy acquire libraries and includes to micro-manager 3rdparty
-if %config% == Release copy %acquire-install-dir%\lib\*.* %mm-3rdparty-dir%\lib
-if %config% == Debug copy %acquire-install-dir%\lib\*.* %mm-3rdparty-dir%\Debug\lib
+if %config% == Release xcopy %acquire-install-dir%\lib\*.* %mm-3rdparty-dir%\lib /Y
+if %config% == Debug xcopy %acquire-install-dir%\lib\*.* %mm-3rdparty-dir%\Debug\lib /Y
 xcopy %acquire-install-dir%\include\*.* %mm-3rdparty-dir%\include /s /Y
 
 Rem build micro-manager Acquire adapter
